c1c95926103e915cc79ad7c8818f861298e19331
Eremel <richardbailey@outlook.com>
2024-10-29 09:26:52 +0000

    Malverk compat

---
+++ localization/default.lua
@@ -0,0 +1,21 @@
+return {
+    descriptions = {
+        alt_texture = {
+            alt_tex_aure_spec_aure = {
+                name = 'Aure',
+                c_aura = {
+                    name = 'Aure'
+                }
+            }
+        },
+        texture_packs = {
+            texpack_aure_spec_aure = {
+                name = 'Aure',
+                text = {
+                    'Replaces the {C:spectral}Aura{} card',
+                    'with {C:dark_edition}Aure'
+                }
+            }
+        }
+    }
+}
